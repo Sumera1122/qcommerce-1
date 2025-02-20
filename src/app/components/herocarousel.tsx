baae56582c7ba3252ab2332afa18863fc755893b
sumera shadab <sumeramushtaque993@gmail.com>
2025-02-20 23:53:42 +0500

    Your commit message here

--- src/app/components/herocarousel.tsx
+++ src/app/components/herocarousel.tsx
@@ -148,7 +148,7 @@
 
 
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { motion } from "framer-motion";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
